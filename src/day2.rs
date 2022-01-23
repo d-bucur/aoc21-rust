bc5f3a48a7e5645fd7ec13eb3b8f5a4fdcfe6479
Dorian Bucur <sirdorius@gmail.com>
2022-01-23 22:15:08 +0100

    solved day2 p2

--- src/day2.rs
+++ src/day2.rs
@@ -2,18 +2,46 @@ use regex::Regex;
 
 use crate::common::read_lines;
 
+fn instruction_re() -> Regex {
+    Regex::new(r"(\w+) (\d+)").unwrap()
+}
+
 pub fn part1() {
-    let re = Regex::new(r"(\w+) (\d+)").unwrap();
+    let re = instruction_re();
     let mut horizontal = 0i32;
     let mut depth = 0i32;
 
     for line in read_lines() {
         let cap = re.captures(&line).unwrap();
         let dist = cap[2].parse::<i32>().unwrap();
-        match cap[1].as_ref() {
+        match &cap[1] {
             "forward" => horizontal += dist,
             "down" => depth += dist,
             "up" => depth -= dist,
+            _ => (),
+        };
+    }
+
+    println!("{}", horizontal * depth);
+}
+
+
+pub fn part2() {
+    let re = instruction_re();
+    let mut horizontal = 0i32;
+    let mut depth = 0i32;
+    let mut aim = 0i32;
+
+    for line in read_lines() {
+        let cap = re.captures(&line).unwrap();
+        let val = cap[2].parse::<i32>().unwrap();
+        match &cap[1] {
+            "forward" => {
+                horizontal += val;
+                depth += aim * val;
+            },
+            "down" => aim += val,
+            "up" => aim -= val,
             _ => (),
         };
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 mod common;
 
+mod day2;
+use day2::part2;
+
 fn main() {
+    part2();
 }
