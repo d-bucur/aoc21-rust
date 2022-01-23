705992da1d7c1d956192150cdb5181d3228deb82
Dorian Bucur <sirdorius@gmail.com>
2022-01-23 21:46:05 +0100

    refactor day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,13 +1,11 @@
-use std::io::{self, BufRead};
+use crate::common::read_lines;
 
 pub fn part1() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
     let mut larger_count = 0u32;
     let mut last_read: Option<u32> = None;
 
-    while let Some(line) = lines.next() {
-        let depth = line.unwrap().parse::<u32>().unwrap();
+    for line in read_lines() {
+        let depth = line.parse::<u32>().unwrap();
         if last_read.is_some() && last_read.unwrap() < depth {
             larger_count += 1;
         }
@@ -17,24 +15,24 @@ pub fn part1() {
 }
 
 pub fn part2() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
     let mut larger_count = 0u32;
     const WINDOW_SIZE: usize = 3;
     let mut window: [u32; WINDOW_SIZE] = [0; 3];
     let mut window_sum = 0u32;
     let mut last_idx: usize = 0;
 
+    let mut lines_it = read_lines();
+
     for i in 0..WINDOW_SIZE {
-        if let Some(line) = lines.next() {
-            let depth = line.unwrap().parse::<u32>().unwrap();
+        if let Some(line) = lines_it.next() {
+            let depth = line.parse::<u32>().unwrap();
             window[i] = depth;
             window_sum += depth;
         }
     }
 
-    while let Some(line) = lines.next() {
-        let depth = line.unwrap().parse::<u32>().unwrap();
+    for line in lines_it {
+        let depth = line.parse::<u32>().unwrap();
         let new_window_sum = window_sum + depth - window[last_idx];
         if new_window_sum > window_sum {
             larger_count += 1;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 mod common;
 
-mod day2;
-use day2::part1;
-
 fn main() {
-    part1();
 }
