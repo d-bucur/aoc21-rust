fb7002f2a3d66fc640cc415e6e37996397c32d43
Dorian Bucur <sirdorius@gmail.com>
2022-01-25 21:38:41 +0100

    day 7 part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -17,22 +17,41 @@ pub fn part1() {
         positions.iter().min().unwrap(),
         positions.iter().max().unwrap(),
     );
-
-    let dist_min = (a..b).map(|p| distance(&positions, p)).min().unwrap();
+    let dist_min = (a..b).map(|p| distance_simple(&positions, p)).min().unwrap();
     println!("{dist_min}");
-
-    // let avg = positions.iter().sum::<u32>() as f32 / positions.len() as f32;
-    // println!("{avg}")
 }
 
-fn distance(positions: &[u32], t: u32) -> u32 {
+fn distance_simple(positions: &[u32], t: u32) -> u32 {
     positions
         .iter()
         .map(|&p| (p as i32 - t as i32).abs() as u32)
         .sum::<u32>()
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let positions = read_input();
+    let (&a, &b) = (
+        positions.iter().min().unwrap(),
+        positions.iter().max().unwrap(),
+    );
+    let mut distances = vec![0u32; b as usize + 1];
+    for i in 1..(b as usize + 1) {
+        distances[i] = distances[i-1] + i as u32;
+    }
+
+    let dist_min = (a..b).map(|p| distance_exp(&positions, p, &distances)).min().unwrap();
+    println!("{dist_min}");
+}
+
+fn distance_exp(positions: &[u32], t: u32, distances: &[u32]) -> u32 {
+    positions
+        .iter()
+        .map(|&p| {
+            let d = (p as i32 - t as i32).abs() as usize;
+            distances[d]
+        })
+        .sum::<u32>()
+}
 
 fn main() {
     let part = env::args().nth(1).unwrap();
