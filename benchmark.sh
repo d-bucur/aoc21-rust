ee78a788d6eaefd1faff14106a27d5f405bb726e
Dorian Bucur <sirdorius@gmail.com>
2022-01-25 20:43:13 +0100

    format table names in benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cargo build --release
 
+format_table() {
+  sed -i 's/`.*release\/day\([0-9]\) \([0-9]\)`/[day \1 part \2](\/src\/bin\/day\1.rs)/g' results.md
+}
+
 if [ "$1" = "all" ]; then
   hyperfine\
  --warmup 3\
@@ -8,6 +12,7 @@ if [ "$1" = "all" ]; then
  "cat inputs/day{day}.input | ./target/release/day{day} 1"\
  "cat inputs/day{day}.input | ./target/release/day{day} 2"\
  --export-markdown results.md
+ format_table
 else
   hyperfine --warmup 3 "cat inputs/day${1}.input | ./target/release/day${1} ${2}"
 fi
